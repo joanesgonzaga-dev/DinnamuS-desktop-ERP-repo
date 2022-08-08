03b8317bacc61851d9cf5e306b6359e7226b546e
Joanes Gonzaga <joanesgonzaga.dev@gmail.com>
2022-08-07 23:53:51 -0300

    Initial Commit - Desktop ERP Versioning Controll

---
+++ View/Control/CRUDButtonsControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DinnamuS_2._0_Desktop.View.Control
+{
+    /// <summary>
+    /// Interação lógica para CRUDButtonsControl.xaml
+    /// </summary>
+    public partial class CRUDButtonsControl : UserControl
+    {
+        public CRUDButtonsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
